13f71ac9d0b0e38d6873881995ef7d9cf09f3405
YauheniButski <ybutski@gmail.com>
2020-03-31 23:03:16 +0300

    1. Prepare 'build' folder of Client 2. Setup server for using a Client as static folder and run all on one port localhost:5000

---
+++ client/build/precache-manifest.b2892f9b35bc1cff465e78bb9a8fb768.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c500db1d66bc7a9f891e7a0294078b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2327317d7cd2bfceab0",
+    "url": "/static/css/main.bee9c92c.chunk.css"
+  },
+  {
+    "revision": "713e9ab612b18f06ad7b",
+    "url": "/static/js/2.50617628.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/static/js/2.50617628.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2327317d7cd2bfceab0",
+    "url": "/static/js/main.45e98719.chunk.js"
+  },
+  {
+    "revision": "20c94660fed1dea8bbb7",
+    "url": "/static/js/runtime-main.1d510b63.js"
+  }
+]);
